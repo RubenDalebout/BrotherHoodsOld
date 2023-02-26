632f494554a352897c0637de477dd03d34cea6fd
DeEchteZeeuw <rubendalebout@gmail.com>
2023-02-26 15:09:00 +0100

    Set tab completer for the commands

--- src/main/java/io/github/rubendalebout/brotherhoods/managers/CommandManager.java
+++ src/main/java/io/github/rubendalebout/brotherhoods/managers/CommandManager.java
@@ -3,10 +3,19 @@ package io.github.rubendalebout.brotherhoods.managers;
 import io.github.rubendalebout.brotherhoods.BrotherHoods;
 import io.github.rubendalebout.brotherhoods.commands.Kingdom;
 import io.github.rubendalebout.brotherhoods.commands.Kingdoms;
+import org.bukkit.command.PluginCommand;
+
 public class CommandManager {
 
     public CommandManager(BrotherHoods plugin) {
-        plugin.getCommand("kingdom").setExecutor(new Kingdom(plugin));
-        plugin.getCommand("kingdoms").setExecutor(new Kingdoms(plugin));
+        PluginCommand kdCommand = plugin.getCommand("kingdom");
+        Kingdom kingdom = new Kingdom(plugin);
+        kdCommand.setExecutor(kingdom);
+        kdCommand.setTabCompleter(kingdom);
+
+        PluginCommand kdsCommand = plugin.getCommand("kingdoms");
+        Kingdoms kingdoms = new Kingdoms(plugin);
+        kdsCommand.setExecutor(kingdoms);
+        kdsCommand.setTabCompleter(kingdoms);
     }
 }
