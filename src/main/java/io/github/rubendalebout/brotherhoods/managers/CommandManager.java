02989022c89ee48aceeeee8fa3049b542da903be
DeEchteZeeuw <rubendalebout@gmail.com>
2023-02-27 19:29:18 +0100

    Added the command executor and completer for brotherhood

--- src/main/java/io/github/rubendalebout/brotherhoods/managers/CommandManager.java
+++ src/main/java/io/github/rubendalebout/brotherhoods/managers/CommandManager.java
@@ -1,6 +1,7 @@
 package io.github.rubendalebout.brotherhoods.managers;
 
 import io.github.rubendalebout.brotherhoods.BrotherHoods;
+import io.github.rubendalebout.brotherhoods.commands.Brotherhood;
 import io.github.rubendalebout.brotherhoods.commands.Brotherhoods;
 import io.github.rubendalebout.brotherhoods.commands.Kingdom;
 import io.github.rubendalebout.brotherhoods.commands.Kingdoms;
@@ -9,6 +10,11 @@ import org.bukkit.command.PluginCommand;
 public class CommandManager {
 
     public CommandManager(BrotherHoods plugin) {
+        PluginCommand bhCommand = plugin.getCommand("brotherhood");
+        Brotherhood brotherhood = new Brotherhood(plugin);
+        bhCommand.setExecutor(brotherhood);
+        bhCommand.setTabCompleter(brotherhood);
+
         PluginCommand bhsCommand = plugin.getCommand("brotherhoods");
         Brotherhoods brotherhoods = new Brotherhoods(plugin);
         bhsCommand.setExecutor(brotherhoods);
